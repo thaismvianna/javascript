1219949d62c50cec6f1e91a28791a312bedf0087
thaismvianna <thaismvianna@outlook.com>
2025-04-03 22:20:52 -0300

    repeticoes

---
+++ aula08/script.js
@@ -0,0 +1,36 @@
+/*
+console.log("Tudo bem?")
+console.log("Tudo bem?")
+console.log("Tudo bem?")
+console.log("Tudo bem?")
+console.log("Tudo bem?")
+console.log("Tudo bem?")
+*/
+
+// while = o teste lógico é feito no inicio
+/* var c = 1
+while (c <= 6) {
+    console.log(`Passo ${c}`)
+    c++ // mesma coisa que c = c + 1
+
+} */
+
+// do...while = o teste lógico é feito no final
+/* var c = 1
+do {
+    console.log(`Passo ${c}`)
+    c++
+} while (c <= 6) */
+
+/* for = teste lógico feito com incremento, ate ficar falso e encerrar
+var c = 1 // seria a inicialização
+while (c <= 5) { // seria a condição
+    console.log(c)
+    c++ // seria o incremento
+} */
+
+console.log("Vai começar...")
+for (var c=1; c<=5; c++) {
+    console.log(c)
+}
+console.log("FIM!")
